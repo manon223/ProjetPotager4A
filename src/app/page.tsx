4d61db29b4a059c87a9f65a5140a9850c429e97e
manon <manon.bacque@epfedu.fr>
2025-03-31 18:08:21 +0200

    Modification du home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@ export default function Home(){
   return (
       <>
         <Navbar/>
-        <main style={{ marginTop: '80px' }}> {/* Marge pour éviter que la navbar recouvre le titre */}
-          <h1>Home</h1>
+        <main style={{ marginTop: '100px' }}> {/* Marge pour éviter que la navbar recouvre le titre */}
+
+            <div className="grid grid-cols-2 gap-4 w-full max-w-4xl">
+                <div className="bg-green-100 p-4 rounded-lg text-center">
+                <h2 className="font-semibold">Météo</h2>
+                    <p>Cachan</p>
+                </div>
+
+            </div>
         </main>
       </>
 
